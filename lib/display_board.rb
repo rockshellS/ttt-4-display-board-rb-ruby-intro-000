084eb3b008bfb083e9dd3dc44079662b4c5d3803
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-08-26 15:53:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ board = [" "," "," "," ","X"," "," "," "," "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts " O | X |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
