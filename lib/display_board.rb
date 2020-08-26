48e8ed41ef5102716eccc7eba4fb89ada7c228dc
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-08-26 15:52:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,9 @@ board = [" "," "," "," ","X"," "," "," "," "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts " O | X |   "
   puts "-----------"
   puts "   |   |   "
-  puts "x#{board[4]},o#{board[0]},"
 end
   
 
